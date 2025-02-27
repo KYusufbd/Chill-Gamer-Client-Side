19f81fb1b4db7c50106c40bd60ff87b60d757fb3
Kazi Yusuf <yfaka001@gmail.com>
2025-02-27 22:57:17 +0600

    Watchlist page is ready

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -8,15 +8,54 @@ const Watchlist = () => {
   const { watchlist } = useContext(LoadingContext);
   const navigate = useNavigate();
 
-  // Testing purposes
-  console.log(watchlist);
-
   !user && navigate("/login");
 
   if (user) {
     return (
-      <div>
-        <h1 className="text-5xl text-center">This is watchlist page</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Publishing Year</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {watchlist.map((game) => {
+              return (
+                <tr key={game._id}>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src={game.image}
+                            alt={game.title}
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{game.title}</div>
+                        <div className="text-sm opacity-50">{game.genre}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    {game.description}
+                    <br />
+                  </td>
+                  <td>{game.publishing_year}</td>
+                  <th>
+                    <button className="btn btn-ghost">Remove</button>
+                  </th>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
